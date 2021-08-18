fc7a20c656a779ac847185288a7c7afab5eb1ea6
Larry Fenn <larry.fenn@gmail.com>
2021-08-17 23:20:25 -0400

    Trim unused libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,4 @@
 #include <Adafruit_Protomatter.h>
-#include <algorithm>
-#include <array>
-#include <cstdint>
-#include <functional>
 #include <ArduinoGL.h>
 #include <Adafruit_GFX.h>
 
@@ -12,27 +8,26 @@ uint8_t clockPin   = 14;
 uint8_t latchPin   = 15;
 uint8_t oePin      = 16;
 
+// 6 bit color - 64 brightness levels for each of R, G, B.
 Adafruit_Protomatter screen(
-  64, 6, 1, rgbPins, 5, addrPins, clockPin, latchPin, oePin, false);
-GFXcanvas1 c(64, 64);
+  64, 6, 1, rgbPins, 5, addrPins, clockPin, latchPin, oePin, true);
 
+GFXcanvas1 c(64, 64);
 void setup(void) {
   Serial.begin(9600);
-
   ProtomatterStatus status = screen.begin();
   Serial.print("Protomatter begin() status: ");
   Serial.println((int)status);
   if (status != PROTOMATTER_OK) {
     for (;;);
   }
-  glUseCanvas(c);
+  glUseCanvas(&c);
   glClear(GL_COLOR_BUFFER_BIT);
   glPointSize(4);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-//  glOrtho(-5, 5, -5, 5, 0.1, 999.f);
-  gluPerspective(30.0, c.width()/c.height(), 0.1f, 9999.f);
-    
+  //glOrtho(-5, 5, -5, 5, 0.1, 9999.f);
+  gluPerspective(30.0, c.width()/c.height(), 0.1f, 999.f);
   glMatrixMode(GL_MODELVIEW);
 }
 
@@ -83,6 +78,9 @@ void drawCube() {
 
 void loop(void) {
   float angle = (millis() / 10 % 360) * 1.0f;
+  //float in_data = analogRead(A0);
+  //Serial.println(in_data);
+  //float angle = (in_data * 360.0f) / 1023;
   const float scale = 2.5;
 
   glClear(GL_COLOR_BUFFER_BIT); 
@@ -93,6 +91,7 @@ void loop(void) {
   glScalef(scale, scale, scale);
   drawCube();
   screen.fillScreen(screen.color565(0, 0, 0));
-  screen.drawBitmap(0, 0, c.getBuffer(), 64, 64, 0xFFFF);
+  // the darkest that can be rendered apparently is screen.color565(8, 8, 8)
+  screen.drawBitmap(0, 0, c.getBuffer(), 64, 64, screen.color565(8, 8, 8));
   screen.show();
 }
