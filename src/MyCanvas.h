c9607f2392512b61a8f5369862921596cc59ccc7
Yourself <kyle.smith314@gmail.com>
2021-08-18 19:36:15 -0600

    color interpolation and other fixes

--- src/MyCanvas.h
+++ src/MyCanvas.h
@@ -1,5 +1,11 @@
+#include "color.h"
 #include <Adafruit_GFX.h>
 
+#ifndef MyCanvas_h
+#define MyCanvas_h
+
 class MyCanvas : public GFXcanvas16 {
-  void drawShadedLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t start_color, uint16_t end_color); 
-}
+    void drawShadedLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, Color565 start_color, Color565 end_color);
+};
+
+#endif
---
+++ src/color.h
@@ -0,0 +1,30 @@
+#include <cstdint>
+
+#ifndef Color_h
+#define Color_h
+
+class Color565 {
+  public:
+    Color565();
+    Color565(uint8_t r, uint8_t g, uint8_t b);
+    explicit Color565(uint16_t c);
+
+    operator uint16_t() const;
+
+    uint8_t r() const;
+    uint8_t g() const;
+    uint8_t b() const;
+
+    uint8_t r5() const;
+    uint8_t g6() const;
+    uint8_t b5() const;
+
+    Color565 with_r(uint8_t r) const;
+    Color565 with_g(uint8_t g) const;
+    Color565 with_b(uint8_t b) const;
+
+  private:
+    uint16_t m_color;
+};
+
+#endif
