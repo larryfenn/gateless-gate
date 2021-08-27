907881079d81758e3ebfd74fc035c42f43021f0d
Yourself <kyle.smith314@gmail.com>
2021-08-26 22:27:16 -0600

    avoid bounds checks and rotation

--- src/MyCanvas.h
+++ src/MyCanvas.h
@@ -10,6 +10,8 @@ class MyCanvas : public GFXcanvas16 {
         MyCanvas(int16_t width, int16_t height);
 
     void drawShadedLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, Color565 start_color, Color565 end_color);
+
+    uint16_t &pixel(int16_t x, int16_t y) { return getBuffer()[x + y * WIDTH]; }
 };
 
 #endif
